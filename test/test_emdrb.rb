5e73cecfbecb99aaa4b5690a0758ca30925072ec
Rafael Sevilla <dido@imperium.ph>
2008-12-02 03:59:14 +0000

    basic functionality tests

---
+++ test/test_emdrb.rb
@@ -0,0 +1,64 @@
+#
+# Author:: Rafael R. Sevilla (mailto:dido@imperium.ph)
+# Copyright:: Copyright (c) 2008 Rafael R. Sevilla
+# Homepage:: http://emdrb.rubyforge.org/
+# License:: GNU General Public License / Ruby License
+#
+# $Id$
+#
+#----------------------------------------------------------------------------
+#
+# Copyright (C) 2008 Rafael Sevilla
+# This file is part of EMDRb
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of either: 1) the GNU General Public License
+# as published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version; or 2) Ruby's License.
+# 
+# See the file COPYING for complete licensing information.
+#----------------------------------------------------------------------------
+#
+require 'test/unit'
+require 'emdrb/emdrb'
+require 'drb'
+
+Thread.abort_on_exception = true
+
+class TestServer
+  def identity(x)
+    return(x)
+  end
+
+  def addtwo(x, y)
+    return(x+y)
+  end
+
+  def sum(*vals)
+    return(vals.inject(0) { |x,y| x + y })
+  end
+
+  def blockyield(*vals)
+    vals.each do |x|
+      yield x
+    end
+  end
+end
+
+class EMDRbTest < Test::Unit::TestCase
+  def test_drb
+    EMDRb.start_service("druby://:12345", TestServer.new)
+    o = DRbObject.new_with_uri("druby://localhost:12345")
+    DRb.start_service
+    assert_equal(1, o.identity(1))
+    assert_equal(3, o.addtwo(1, 2))
+    assert_raises(ArgumentError) do
+      o.addtwo(1)
+    end
+    assert_equal(15, o.sum(1,2,3,4,5))
+    val = 1
+    o.blockyield(1,2,3,4,5,6,7) { |x| val *= x }
+    assert_equal(5040, val)
+  end
+
+end

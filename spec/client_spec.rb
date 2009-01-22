3a46a1c7cf8a9218947f82ef3f51797e27af43ab
Rafael Sevilla <dido@imperium.ph>
2009-01-22 10:38:44 +0000

    DRb revisions

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -27,35 +27,46 @@ class TestServer
   end
 end
 
-describe "EMDRbClient" do
+describe EMDRb, "Client" do
   before(:all) do
     DRb.start_service("druby://:12345", TestServer.new)
-    EMDRb.start_service
-    @obj = EMDRb::DRbObject.new(nil, "druby://localhost:12345")
-    @q = Queue.new
+    @obj = DRb::DRbObject.new(nil, "druby://localhost:12345")
+  end
+
+  it "should be able to perform simple synchronous method calls" do
+    @obj.identity(1).should == 1
+    @obj.addtwo(1, 2).should == 3
+  end
+
+  it "should be able to perform synchronous method calls with a block" do
+    val = 1
+    @obj.blockyield(1,2,3,4,5,6,7) { |x| val *= x }
+    val.should == 5040
   end
 
   it "should be able to perform asynchronous method calls" do
+    q = Queue.new
     EventMachine::next_tick do
       @obj.send_async(:identity, 1).callback do |data|
         data[0].should be_true
         data[1].should == 1
-        @q << data
+        q << data
       end
     end
-    @q.shift
+    q.shift
   end
 
   it "should be able to perform asynchronous method calls with a passed block" do
     EventMachine::next_tick do
-      val = 1
+      q = Queue.new
       df = @obj.send_async(:blockyield, 1,2,3,4,5,6,7) { |x| val *= x; val }
       df.callback do |data|
         data[0].should be_true
-        @q << data
+        q << data
       end
-      @q.shift
+      q.shift
       val.should == 5040
     end
   end
+
 end

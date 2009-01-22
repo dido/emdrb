cd30d4aa6cb357ef5638df8dbe7eed740e581211
Rafael Sevilla <dido@imperium.ph>
2009-01-22 10:42:27 +0000

    client specs revised

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,5 +1,23 @@
 #
+# Author:: Rafael R. Sevilla (mailto:dido@imperium.ph)
+# Copyright:: Copyright © 2008, 2009 Rafael R. Sevilla
+# Homepage:: http://emdrb.rubyforge.org/
+# License:: GNU General Public License / Ruby License
+#
 # $Id$
+#
+#----------------------------------------------------------------------------
+#
+# Copyright © 2008, 2009 Rafael Sevilla
+# This file is part of EMDRb
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of either: 1) the GNU General Public License
+# as published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version; or 2) Ruby's License.
+# 
+# See the file COPYING for complete licensing information.
+#----------------------------------------------------------------------------
 #
 require File.join(File.dirname(__FILE__), %w[spec_helper])
 require 'drb'

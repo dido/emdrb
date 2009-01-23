2540971ad083c59bc7e2e162256c0ddc05834f2b
Rafael Sevilla <dido@imperium.ph>
2009-01-23 08:48:43 +0000

    don't use the spec helper so that we really make use of DRb here, and not EMDRb.  This is an interop test and we run EMDRb's server code in drbserver.rb

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -19,57 +19,33 @@
 # See the file COPYING for complete licensing information.
 #----------------------------------------------------------------------------
 #
-require File.join(File.dirname(__FILE__), %w[spec_helper])
-require 'drb'
+# This tests the EMDRb server implementation against the standard DRb
+# client.
+#
+#require File.join(File.dirname(__FILE__), %w[spec_helper])
+require File.join(File.dirname(__FILE__), %w[spec_common])
+require 'drb'       # yes, that's right, we use the STANDARD DRb here!
+require 'thread'
+
 Thread.abort_on_exception = true
 
-class TestServer
-  def identity(x)
-    return(x)
-  end
+describe "EMDRb Server" do
+  it_should_behave_like "DRb basics"
 
-  def addtwo(x, y)
-    return(x+y)
-  end
-
-  def sum(*vals)
-    return(vals.inject(0) { |x,y| x + y })
-  end
-
-  def blockyield(*vals)
-    vals.each do |x|
-      yield x
-    end
-  end
-end
-
-describe EMDRb, "Server" do
   before(:all) do
-    DRb.start_service("druby://:12345", TestServer.new)
+    # but we start the *server* with EMDRb
+    system(File.join(File.dirname(__FILE__), "drbserver.rb emdrb"))
+    DRb.start_service
     @obj = DRbObject.new_with_uri("druby://localhost:12345")
   end
 
-  it "should handle basic method calls from the standard DRb client" do
-    @obj.identity(1).should == 1
-    @obj.addtwo(1, 2).should == 3
-  end
-
-  it "should raise an error if insufficient arguments are provided to a remote method call" do
-    lambda { @obj.addtwo(1) }.should raise_error(ArgumentError)
+  after(:all) do
+    pid = File.open(File.join(File.dirname(__FILE__), "drbserver.pid")) { |fp| fp.read.to_i }
+    Process.kill("SIGTERM", pid)
   end
 
   it "should work with variadic methods" do
     @obj.sum(1,2,3,4,5).should == 15
-  end
-
-  it "should allow blocks to be passed" do
-    val = 1
-    @obj.blockyield(1,2,3,4,5,6,7) { |x| val *= x }
-    val.should == 5040
-  end
-
-  after(:all) do
-    DRb.thread.kill
   end
 
 end

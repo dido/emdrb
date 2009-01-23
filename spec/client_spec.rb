d816a0d27f8aca9d50dd231efd041abd4e99fa6a
Rafael Sevilla <dido@imperium.ph>
2009-01-23 03:40:15 +0000

    no need to resync asynchronous calls

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -51,6 +51,26 @@ describe EMDRb, "Client" do
     @obj = DRb::DRbObject.new(nil, "druby://localhost:12345")
   end
 
+  it "should be able to perform asynchronous method calls" do
+    EventMachine::next_tick do
+      @obj.send_async(:identity, 1).callback do |data|
+        data[0].should be_true
+        data[1].should == 1
+      end
+    end
+  end
+
+  it "should be able to perform asynchronous method calls with a passed block" do
+    EventMachine::next_tick do
+      val = 1
+      df = @obj.send_async(:blockyield, 1,2,3,4,5,6,7) { |x| val *= x; val }
+      df.callback do |data|
+        data[0].should be_true
+        val.should == 5040
+      end
+    end
+  end
+
   it "should be able to perform simple synchronous method calls" do
     @obj.identity(1).should == 1
     @obj.addtwo(1, 2).should == 3
@@ -60,31 +80,6 @@ describe EMDRb, "Client" do
     val = 1
     @obj.blockyield(1,2,3,4,5,6,7) { |x| val *= x }
     val.should == 5040
-  end
-
-  it "should be able to perform asynchronous method calls" do
-    q = Queue.new
-    EventMachine::next_tick do
-      @obj.send_async(:identity, 1).callback do |data|
-        data[0].should be_true
-        data[1].should == 1
-        q << data
-      end
-    end
-    q.shift
-  end
-
-  it "should be able to perform asynchronous method calls with a passed block" do
-    EventMachine::next_tick do
-      q = Queue.new
-      df = @obj.send_async(:blockyield, 1,2,3,4,5,6,7) { |x| val *= x; val }
-      df.callback do |data|
-        data[0].should be_true
-        q << data
-      end
-      q.shift
-      val.should == 5040
-    end
   end
 
 end
